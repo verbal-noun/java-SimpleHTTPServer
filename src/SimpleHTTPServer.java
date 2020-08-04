0e0e11b439c410b82e25033bb4b23854b2a64067
verbal-noun <kaifahsan420@gmail.com>
2020-08-04 11:58:27 +1000

    Creating a network server

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -1,2 +1,12 @@
+import java.net.ServerSocket;
+
 public class SimpleHTTPServer {
+    public static void main(String[] args) throws Exception {
+        // Creating a network socket
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while(true) {
+            // spin forever
+        }
+    }
 }
